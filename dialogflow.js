c345449502a949e425dea7a9eb7072ac691fb123
phamhoa2192k <phamhoa2192k@gmail.com>
2021-05-31 17:17:25 +0700

    initilize project

---
+++ dialogflow.js
@@ -0,0 +1,68 @@
+// Imports the Dialogflow library
+const dialogflow = require('@google-cloud/dialogflow');
+
+// Instantiates a session client
+const sessionClient = new dialogflow.SessionsClient();
+
+async function detectIntent(
+  projectId,
+  sessionId,
+  query,
+  contexts,
+  languageCode
+) {
+  // The path to identify the agent that owns the created intent.
+  const sessionPath = sessionClient.projectAgentSessionPath(
+    projectId,
+    sessionId
+  );
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: query,
+        languageCode: languageCode,
+      },
+    },
+  };
+
+  if (contexts && contexts.length > 0) {
+    request.queryParams = {
+      contexts: contexts,
+    };
+  }
+
+  const responses = await sessionClient.detectIntent(request);
+  return responses[0];
+}
+
+async function executeQueries(projectId, sessionId, query, languageCode) {
+  // Keeping the context across queries let's us simulate an ongoing conversation with the bot
+  let context;
+  let intentResponse;
+  try {
+    console.log(`Sending Query: ${query}`);
+    intentResponse = await detectIntent(
+      projectId,
+      sessionId,
+      query,
+      context,
+      languageCode
+    );
+    // console.log('Detected intent');
+    // console.log(
+    //   `Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`
+    // );
+    // // Use the context from this response for next queries
+    // context = intentResponse.queryResult.outputContexts;
+    // console.log(intentResponse.queryResult.intent.displayName)
+  } catch (error) {
+    console.log(error);
+  }
+  return intentResponse
+}
+
+module.exports = executeQueries
+//executeQueries(projectId, sessionId, query, languageCode);
---
+++ routes/query.js
@@ -0,0 +1,17 @@
+const PROJECTID = process.env.PROJECTID
+const SESSIONID = process.env.SESSIONID
+const LANGUAGECODE = process.env.LANGUAGECODE
+const executeQueries = require("../dialogflow.js")
+const query = require("express").Router()
+
+query.get("/", (req, res) => {
+    let text = req.query.q
+    executeQueries(PROJECTID, SESSIONID, text, LANGUAGECODE)
+        .then(res => res)
+        .then(json => res.status(200).send(JSON.stringify({
+            text: json.queryResult.fulfillmentMessages[0].text.text
+        })))
+
+})
+
+module.exports = query
